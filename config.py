15f9e1af19f320ad7b5e398e6412bee5988767ac
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-14 08:02:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,39 +1,40 @@
-import re, os, time
+import os
+import time
+import re
 
-id_pattern = re.compile(r'^.\d+$') 
+id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
-    # pyro client config
-    API_ID    = os.environ.get("API_ID", "23990433")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6237026610:AAHhvQbM1nH4bDCAw80Dmx9rCmeGBbMdmQg") # ⚠️ Required
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "autofilter2_bot") # ⚠️ Required
-   
-    # database config
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://sankar:sankar@sankar.lldcdsx.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
- 
-    # other configs
+    # Pyrogram client config
+    API_ID = os.environ.get("API_ID", "23990433")  # ⚠️ Required
+    API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")  # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6237026610:AAHhvQbM1nH4bDCAw80Dmx9rCmeGBbMdmQg")  # ⚠️ Required
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "autofilter2_bot")  # ⚠️ Required
+    
+    # Database config
+    DB_URL = os.environ.get("DB_URL", "mongodb+srv://sankar:sankar@sankar.lldcdsx.mongodb.net/?retryWrites=true&w=majority")  # ⚠️ Required
+    
+    # Other configs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     TG_MAX_SIZE = 2040108421
-    BOT_UPTIME  = time.time()
-    START_PIC   = os.environ.get("START_PIC", "")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5821871362').split()] # ⚠️ Required
-    FORCE_SUB_TEXT = os.environ.get('FORCE_SUB_TEXT', "**Yᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ ᴏᴜʀ Bᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ ɢɪᴠᴇɴ ʙᴇʟᴏᴡ sᴏ ʏᴏᴜ ᴅᴏɴ'ᴛ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇ...\n\nIғ ʏᴏᴜ ᴡᴀɴᴛ ᴛʜᴇ ғɪʟᴇ, ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ '❆ Jᴏɪɴ Oᴜʀ Bᴀᴄᴋ-Uᴘ Cʜᴀɴɴᴇʟ ❆' ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ ᴀɴᴅ ᴊᴏɪɴ ᴏᴜʀ ʙᴀᴄᴋ-ᴜᴘ ᴄʜᴀɴɴᴇʟ, ᴛʜᴇɴ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ '↻ Tʀʏ Aɢᴀɪɴ' ʙᴜᴛᴛᴏɴ ʙᴇʟᴏᴡ...\n\nTʜᴇɴ ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ ᴛʜᴇ ғɪʟᴇs...**")
-    FORCE_SUB = os.environ.get('FORCE_SUB', 'SK_MoviesOffl') # ⚠️ Required
+    BOT_UPTIME = time.time()
+    START_PIC = os.environ.get("START_PIC", "")
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5821871362').split()]  # ⚠️ Required
+    FORCE_SUB_TEXT = os.environ.get('FORCE_SUB_TEXT', "**You are not in our backup channel given below so you don't get the file...\n\nIf you want the file, click on the '❆ Join Our Backup Channel ❆' button below and join our backup channel, then click on the '↻ Try Again' button below...\n\nThen you will get the files...**")
+    FORCE_SUB = os.environ.get('FORCE_SUB', 'SK_MoviesOffl')  # ⚠️ Required
     AUTH_CHANNEL = -1001811608554  # Updated AUTH_CHANNEL value
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001870015374")) # ⚠️ Required
-
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001870015374"))  # ⚠️ Required
     
     # Mega User Account ⚠️ Only Set When you have Pro or Enterprise Mega Account
-    MEGA_EMAIL = os.environ.get("ak14147800@gmail.com")
-    MEGA_PASSWORD = os.environ.get("thakur#12")
-
-    # wes response configuration     
+    MEGA_EMAIL = os.environ.get("MEGA_EMAIL", "ak14147800@gmail.com")
+    MEGA_PASSWORD = os.environ.get("MEGA_PASSWORD", "thakur#12")
+    
+    # Web response configuration
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
     PORT = int(os.environ.get("PORT", "8081"))
 
 class Txt(object):
-    # part of text configuration
+    # Part of text configuration
     START_TXT = """<b>Hello {} 👋,
 ━━━━━━━━━━━━━━━━━━━━━
 This Bot Can Search PornHub
@@ -44,40 +45,39 @@ Link of Mega & YouTube
 ━━━━━━━━━━━━━━━━━━━━━
 ⚠️The Bot Contains 18+ Content
 So Kindly Access it with Your own
-Risk. Children Please Stay Away."
+Risk. Children Please Stay Away.
 We don't intend to spread Pørno-
--graphy here. It's just a bot for a"
-purpose as many of them wanted."
+graphy here. It's just a bot for a
+purpose as many of them wanted.
 ━━━━━━━━━━━━━━━━━━━━━
 Click The Buttons Below To Search
 """
 
     ABOUT_TXT = """<b>╭───────────⍟
-├🤖 ᴍy ɴᴀᴍᴇ : {}
-├👨‍💻 Pʀᴏɢʀᴀᴍᴇʀ : <a href=https://t.me/Snowball_Official>𝓢𝓝𝓞𝓦𝓑𝓐𝓛𝓛</a>
-├👑 Instagram : <a href=https://www.instagram.com/ritesh6_>C-Insta</a> 
-├☃️ ꜰᴏᴜɴᴅᴇʀ ᴏꜰ : <a href=https://t.me/+HzGpLAZXTxoyYTNl>𝖱𝖮𝖮𝖥𝖨𝖵𝖤𝖱𝖲𝖤</a>
-├📕 Lɪʙʀᴀʀy : <a href=https://github.com/pyrogram>Pyʀᴏɢʀᴀᴍ</a>
-├✏️ Lᴀɴɢᴜᴀɢᴇ: <a href=https://www.python.org>Pyᴛʜᴏɴ 3</a>
-├💾 Dᴀᴛᴀ Bᴀꜱᴇ: <a href=https://cloud.mongodb.com>Mᴏɴɢᴏ DB</a>
-├🌀 ᴍʏ ꜱᴇʀᴠᴇʀ : <a href=https://dashboard.heroku.com>Heroku</a>
+├🤖 My name : {}
+├👨‍💻 Programmer : <a href="https://t.me/Snowball_Official">𝓢𝓝𝓞𝓦𝓑𝓐𝓛𝓛</a>
+├👑 Instagram : <a href="https://www.instagram.com/ritesh6_">C-Insta</a>
+├☃️ Founder of : <a href="https://t.me/+HzGpLAZXTxoyYTNl">𝖱𝖮𝖮𝖥𝖨𝖵𝖤𝖱𝖲𝖤</a>
+├📕 Library : <a href="https://github.com/pyrogram">Pyrogram</a>
+├✏️ Language: <a href="https://www.python.org">Python 3</a>
+├💾 Database: <a href="https://cloud.mongodb.com">Mongo DB</a>
+├🌀 My Server : <a href="https://dashboard.heroku.com">Heroku</a>
 ╰───────────────⍟ """
 
     HELP_TXT = """
-Tʜɪs Bᴏᴛ Wɪʟʟ Hᴇʟᴘ Yᴏᴜ Tᴏ Dᴏᴡɴʟᴏᴀᴅ Fᴏʟʟᴏᴡɪɴɢ Fɪʟᴇ ᴛʜʀᴏᴜɢʜ ʟɪɴᴋs:
+This Bot Will Help You To Download Following Files Through Links:
 
 ⊚ YouTube
 ⊚ Mega
 ⊚ PornHub
 
-❗ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/Snowball_official>𝑺𝑼𝑷𝑷𝑶𝑹𝑻</a>
+❗ Any Other Help Contact :- <a href="https://t.me/Snowball_official">Support</a>
 """
 
-
     PROGRESS_BAR = """<b>\n
-╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
-┣⪼ 🗃️ Sɪᴢᴇ: {1} | {2}
-┣⪼ ⏳️ Dᴏɴᴇ : {0}%
-┣⪼ 🚀 Sᴩᴇᴇᴅ: {3}/s
-┣⪼ ⏰️ Eᴛᴀ: {4}
+╭━━━━❰Progress Bar❱━➣
+┣⪼ 🗃️ Size: {1} | {2}
+┣⪼ ⏳️ Done : {0}%
+┣⪼ 🚀 Speed: {3}/s
+┣⪼ ⏰️ ETA: {4}
 ╰━━━━━━━━━━━━━━━➣ </b>"""
