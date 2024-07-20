7df8e2342344a27c22cbb003635862dc887de161
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2024-07-20 23:10:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -119,7 +119,8 @@ async def start_bot():
         "my_account",
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
-        bot_token=Config.BOT_TOKEN
+        bot_token=Config.BOT_TOKEN,
+        plugins=dict(root="plugins"
     )
 
     try:
